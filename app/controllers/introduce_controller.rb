67eb915d96fc3eb1deea4930dceb9505000f7597
Judith Barrett <judith.barrett@gmail.com>
2014-10-21 23:26:58 -0600

    Added my_first_rails_app to new repo

---
+++ app/controllers/introduce_controller.rb
@@ -0,0 +1,10 @@
+class IntroduceController < ApplicationController
+  def introduce
+  end
+
+  def name1
+  end
+
+  def name2
+  end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def hello
+  end
+
+  def name
+  end
+
+end
